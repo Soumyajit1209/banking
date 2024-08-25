56a7a3f5bc09c26be3fc87511b2cc3269b533153
Soumyajit Khan <soumyajitkhan6@gmail.com>
2024-08-25 20:48:54 +0530

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
+
 
 const Home = () => {
+  const loggedIn = {firstName : "Soumyajit"}
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type = "greeting"
+          title = "Welcome"
+          user = {loggedIn?.firstName || 'Guest'}
+          subtext = "Access and manage your account efficiently"
+          />
+          <TotalBalanceBox
+          accounts = {[]}
+          totalBanks = {1}
+          totalCurrentBalance = {1250.35}
+
+          />
+        </header>
+
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
   )
 }
 
